f16c1cca45c9e9ab1fd38aad7299365ed530973f
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-12-06 15:57:12 -0500

    p

--- oop.py
+++ oop.py
@@ -4,6 +4,7 @@ class Podracer:
     self.condition = condition
     self.price = price
 
+ 
   def repair(self):
     self.condition = "repaired"
     
@@ -15,10 +16,18 @@ class AnakinsPod(Podracer):
   def boost(self):
     self.max_speed *= 2
     
-    
+
 class SebulbasPod(Podracer):
   def __init__(self, max_speed, condition, price):
     super.init(max_speed, condition, price):
   
   def flame_jet(self,other):
     other.condition = "trashed"
+    
+"""
+How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
+Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
+How in particular did Object Oriented Programming assist in the solving of this problem?
+
+      
+"""
