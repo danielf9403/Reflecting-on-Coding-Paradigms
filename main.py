66c425c079812a54af3f191d0d84d81ec8bc5d27
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-12-12 15:37:24 -0500

    :D

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ def flatten_and_sort(array):
 
     """
 How does this solution ensure data immutability?
-
+Immutable data types are those, whose values cannot be modified once they are created.
 
 
 Is this solution a pure function? Why or why not?
+Yes. It's defini as a function def.
+
+
 Is this solution a higher order function? Why or why not?
+This isn't Higher Order Function if it contains other functions as a parameter or returns a function as an output.
+
 Would it have been easier to solve this problem using a different programming style?
+NO
+
 Why in particular is functional programming a helpful paradigm when solving this problem?
+Functions produce the same output as the given input, this means they aren't any changes or any other hidden output produced
     """
--- oop.py
+++ oop.py
@@ -26,7 +26,10 @@ class SebulbasPod(Podracer):
     
 """
 How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
+
+
 Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
+
 How in particular did Object Oriented Programming assist in the solving of this problem?
 
       
