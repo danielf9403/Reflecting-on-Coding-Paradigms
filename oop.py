baba083ccff91bbfeb037aa18e2ec80421099ef7
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-12-12 15:39:56 -0500

    :D

--- oop.py
+++ oop.py
@@ -26,11 +26,12 @@ class SebulbasPod(Podracer):
     
 """
 How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
-
+The central pillar of OOPs - data Abstraction, Encapsulation, Inheritance, and Polymorphism, makes it smooth for programmers to solve complex situations.
 
 Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
+no because it would be hard to implement a solution
 
 How in particular did Object Oriented Programming assist in the solving of this problem?
-
+i'm not sure im try to understand it
       
 """
